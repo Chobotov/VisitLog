13fcc41e56cd230a18cf67a1f1971c4912608461
Keliz-hub <keliz@ya.ru>
2019-09-14 23:06:28 +0300

    Add Chobotov branch
    i'm hate android studio....

--- app/src/main/java/com/android/visitlog/Main2Activity.java
+++ app/src/main/java/com/android/visitlog/Main2Activity.java
@@ -2,13 +2,31 @@ package com.android.visitlog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class Main2Activity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener{
+
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        
+
     }
 }
