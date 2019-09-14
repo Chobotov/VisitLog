162cb979df0e035f3e9faa0a932619eddb12ed40
Chobotov <chobotov@bk.ru>
2019-09-14 22:43:11 +0300

    DataBase Added

--- app/src/main/java/com/android/visitlog/MainActivity.java
+++ app/src/main/java/com/android/visitlog/MainActivity.java
@@ -2,13 +2,29 @@ package com.android.visitlog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    DBHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        db = new DBHelper(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
