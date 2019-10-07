3e93beda230b48493d0555ca592c6e8389ac5076
Chobotov <chobotov@bk.ru>
2019-10-07 21:27:46 +0300

    BeginDoPeopleInformationActivity

---
+++ app/src/main/java/com/android/visitlog/People_Information.java
@@ -0,0 +1,14 @@
+package com.android.visitlog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class People_Information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_people__information);
+    }
+}
