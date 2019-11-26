c898042607140d3927e2c9415b7febe8d55a9cc9
Chobotov <chobotov@bk.ru>
2019-11-26 22:04:37 +0300

    FixCalendarFragment

--- app/src/main/java/com/android/visitlog/CalendarFragment.java
+++ app/src/main/java/com/android/visitlog/CalendarFragment.java
@@ -1,110 +1,283 @@
 package com.android.visitlog;
 
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
+import com.prolificinteractive.materialcalendarview.spans.DotSpan;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link CalendarFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link CalendarFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.HashSet;
+
+
 public class CalendarFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public View v;
+    private RecyclerView recyclerView;
+    private MaterialCalendarView mcv;
+    private EventDecorator eventDecorator;
+    private FloatingActionButton fab;
+    private ItemAdapter adapter;
+    private String YEAR, MONTH, DAY;
+    private ArrayList<People> peopleList;
+    private DBHelper dbHelper;
 
-    private OnFragmentInteractionListener mListener;
+    final String LOG_TAG = "myLogs";
 
     public CalendarFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CalendarFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CalendarFragment newInstance(String param1, String param2) {
-        CalendarFragment fragment = new CalendarFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
-    }
+        v = inflater.inflate(R.layout.fragment_calendar, container, false);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        dbHelper = new DBHelper(v.getContext());
+        YEAR = String.valueOf(Calendar.getInstance().get(Calendar.YEAR));
+        MONTH = String.valueOf(Calendar.getInstance().get(Calendar.MONTH) + 1);
+        DAY = String.valueOf(Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+        fab = v.findViewById(R.id.floatingActionButton);
+        mcv = v.findViewById(R.id.calendarView);
+        recyclerView = v.findViewById(R.id.list);
+        eventDecorator = new EventDecorator(getResources().getColor(R.color.colorAccent));
+        eventDecorator.setDates(dbHelper.SelectAllNotEmptyDays(YEAR,MONTH));
+
+        peopleList = new ArrayList<People>();
+
+        FindIDPeopleByData();
+
+        mcv.setDateSelected(CalendarDay.today(),true);
+        mcv.addDecorator(eventDecorator);
+        mcv.invalidateDecorators();
+
+        adapter = new ItemAdapter(v.getContext(), peopleList, new ItemAdapter.ComeLeaveRemove() {
+            @Override
+            public void RemovePeopleData(People people, int position) {
+                dbHelper.DeleteDataFromDataTable(people.Name,YEAR,MONTH,DAY);
+                peopleList.remove(position);
+                eventDecorator.setDates(dbHelper.SelectAllNotEmptyDays(YEAR,MONTH));
+                mcv.invalidateDecorators();
+                adapter.notifyDataSetChanged();
+
+
+
+                //Log.d("delete", people.Name);
+            }
+
+            @Override
+            public void InsertCameTimeInData(People people) {
+                DateFormat df = new SimpleDateFormat("HH:mm");
+                String time = df.format(Calendar.getInstance().getTime());
+                people.CameTime = time;
+                dbHelper.InsertCameTime(people.Name, time, YEAR, MONTH, DAY);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void InsertLeaveTimeInData(People people) {
+                DateFormat df = new SimpleDateFormat("HH:mm");
+                String time = df.format(Calendar.getInstance().getTime());
+                people.LeaveTime = time;
+                dbHelper.InsertLeaveTime(people.Name, time, YEAR, MONTH, DAY);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void CheckTime(People people) {
+                SQLiteDatabase sqLiteDatabase = dbHelper.getReadableDatabase();
+                String id = dbHelper.GetIdByName(people.Name);
+
+                SetTimeToPeople(id,people);
+            }
+        });
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(v.getContext()));
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), PeopleActivity.class);
+                intent.putExtra("year", String.valueOf(YEAR));
+                intent.putExtra("month", String.valueOf(MONTH));
+                intent.putExtra("day", String.valueOf(DAY));
+                startActivity(intent);
+                //update();
+            }
+        });
+
+        mcv.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+                MONTH = String.valueOf(date.getMonth());
+                eventDecorator.setDates(dbHelper.SelectAllNotEmptyDays(YEAR,MONTH));
+                mcv.invalidateDecorators();
+            }
+        });
+
+        mcv.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                YEAR = String.valueOf(date.getYear());
+                MONTH = String.valueOf(date.getMonth());
+                DAY = String.valueOf(date.getDay());
+                update();
+            }
+        });
+        return v;
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+    public void onResume() {
+        super.onResume();
+        update();
+        eventDecorator.setDates(dbHelper.SelectAllNotEmptyDays(YEAR,MONTH));
+        mcv.invalidateDecorators();
+    }
+
+    private void update() {
+        FindIDPeopleByData();
+        if (adapter != null)
+        {
+            adapter.notifyDataSetChanged();
         }
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+
+    private void FindIDPeopleByData() {
+        peopleList.clear();
+        ArrayList<String> id = new ArrayList<>();
+        SQLiteDatabase sqLiteDatabase = dbHelper.getReadableDatabase();
+
+        Cursor c = sqLiteDatabase.rawQuery("SELECT " + dbHelper.ID_PEOPLE
+                        + " FROM " + dbHelper.DATA_PEOPLE
+                        + " WHERE " + dbHelper.YEAR + " = ?"
+                        + " AND " + dbHelper.MONTH + " = ?"
+                        + " AND " + dbHelper.DAY + " = ?"
+                , new String[]{YEAR, MONTH, DAY});
+        if (c.moveToFirst()) {
+            do {
+                String index = c.getString(c.getColumnIndex(dbHelper.ID_PEOPLE));
+                id.add(index);
+            } while (c.moveToNext());
+        } else
+            Log.d(LOG_TAG, "Cursor is null");
+        c.close();
+        FindNamePeopleById(id);
+        sqLiteDatabase.close();
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    private void FindNamePeopleById(ArrayList<String> id) {
+        SQLiteDatabase sqLiteDatabase = dbHelper.getReadableDatabase();
+        Cursor cursor;
+
+        for (String s : id) {
+            cursor = sqLiteDatabase.rawQuery("SELECT " + dbHelper.FULL_NAME
+                    + " FROM " + dbHelper.PEOPLE
+                    + " WHERE " + dbHelper.KEY_ID
+                    + " = ?", new String[]{s});
+            if (cursor.moveToFirst()) {
+                do {
+                    String name = cursor.getString(cursor.getColumnIndex(dbHelper.FULL_NAME));
+                    People people = new People(name);
+                    people.Group = dbHelper.FindGroupThisPeople(people);
+                    peopleList.add(people);
+                } while (cursor.moveToNext());
+            } else
+                Log.d(LOG_TAG, "Cursor is null");
+        }
+        sqLiteDatabase.close();
+    }
+
+    private void SetTimeToPeople(String id,People people){
+        SQLiteDatabase sqLiteDatabase = dbHelper.getReadableDatabase();
+        Cursor cursor;
+
+        cursor = sqLiteDatabase.rawQuery("SELECT " + dbHelper.CAME_TIME
+                + " FROM " + dbHelper.DATA_PEOPLE
+                + " WHERE "
+                + dbHelper.ID_PEOPLE + "=? "
+                + "AND " + dbHelper.YEAR + "=? "
+                + "AND " + dbHelper.MONTH + "=? "
+                + "AND " + dbHelper.DAY + "=? ", new String[]{id, YEAR, MONTH, DAY});
+        if (cursor.moveToFirst()) {
+            int index = cursor.getColumnIndex(dbHelper.CAME_TIME);
+            do {
+                people.CameTime = (cursor.getString(index));
+                //Log.d("people:", "Came " + people.CameTime + " Leave " + people.LeaveTime);
+            } while (cursor.moveToNext());
+        }
+
+        cursor = sqLiteDatabase.rawQuery("SELECT " + dbHelper.LEAVE_TIME
+                + " FROM " + dbHelper.DATA_PEOPLE
+                + " WHERE "
+                + dbHelper.ID_PEOPLE + "=? "
+                + "AND " + dbHelper.YEAR + "=? "
+                + "AND " + dbHelper.MONTH + "=? "
+                + "AND " + dbHelper.DAY + "=? ", new String[]{id, YEAR, MONTH, DAY});
+        if (cursor.moveToFirst()) {
+            int index = cursor.getColumnIndex(dbHelper.LEAVE_TIME);
+            do {
+                people.LeaveTime = (cursor.getString(index));
+                //Log.d("people:", "Came " + people.CameTime + " Leave " + people.LeaveTime);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        sqLiteDatabase.close();
+    }
+
+    public class EventDecorator implements DayViewDecorator {
+
+        private final int color;
+        private HashSet<CalendarDay> dates;
+
+        public EventDecorator(int color) {
+            this.color = color;
+        }
+
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+            return dates.contains(day);
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+            view.addSpan(new DotSpan(5, color));
+        }
+
+        public void setDates(Collection<CalendarDay> dates){
+            this.dates = new HashSet<>(dates);
+        }
     }
 }
--- app/src/main/java/com/android/visitlog/MainActivity.java
+++ app/src/main/java/com/android/visitlog/MainActivity.java
@@ -3,6 +3,8 @@ package com.android.visitlog;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -13,8 +15,10 @@ import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.DayViewDecorator;
@@ -29,241 +33,58 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
+import java.util.GregorianCalendar;
 import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private MaterialCalendarView mcv;
-    private EventDecorator eventDecorator;
-    private FloatingActionButton fab;
-    private ItemAdapter adapter;
-    private String YEAR, MONTH, DAY;
-    private ArrayList<People> peopleList;
-    private DBHelper dbHelper;
-
-    final String LOG_TAG = "myLogs";
+    BottomNavigationView bottomNavigationView;
+    Fragment active;
+    CalendarFragment calendar;
+    PeopleFragment peopleFragment;
+    GroupsFragment groupsFragment;
+    FragmentManager fragmentManager;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dbHelper = new DBHelper(this);
-        YEAR = String.valueOf(Calendar.getInstance().get(Calendar.YEAR));
-        MONTH = String.valueOf(Calendar.getInstance().get(Calendar.MONTH) + 1);
-        DAY = String.valueOf(Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
-        fab = findViewById(R.id.floatingActionButton);
-        mcv = findViewById(R.id.calendarView);
-        recyclerView = findViewById(R.id.list);
-        eventDecorator = new EventDecorator(getResources().getColor(R.color.colorAccent));
-        eventDecorator.setDates(dbHelper.SelectAllNotEmptyDays(YEAR,MONTH));
+        bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
 
-        peopleList = new ArrayList<People>();
+        calendar = new CalendarFragment();
+        //peopleFragment = new PeopleFragment();
+        groupsFragment = new GroupsFragment();
 
-        FindIDPeopleByData();
+        fragmentManager = getSupportFragmentManager();
 
-        mcv.setDateSelected(CalendarDay.today(),true);
-        mcv.addDecorator(eventDecorator);
-        mcv.invalidateDecorators();
-        
-        adapter = new ItemAdapter(this, peopleList, new ItemAdapter.ComeLeaveRemove() {
-            @Override
-            public void RemovePeopleData(People people, int position) {
-                dbHelper.DeleteDataFromDataTable(people.Name,YEAR,MONTH,DAY);
-                peopleList.remove(position);
-                eventDecorator.setDates(dbHelper.SelectAllNotEmptyDays(YEAR,MONTH));
-                mcv.invalidateDecorators();
-                adapter.notifyDataSetChanged();
+        fragmentManager.beginTransaction().add(R.id.main_container, calendar, "calendar").commit();
+//      fragmentManager.beginTransaction().add(R.id.main_container,peopleFragment,"people").hide(peopleFragment).commit();
+        fragmentManager.beginTransaction().add(R.id.main_container, groupsFragment, "groups").hide(groupsFragment).commit();
 
+        active = calendar;
 
-                
-                //Log.d("delete", people.Name);
-            }
-
-            @Override
-            public void InsertCameTimeInData(People people) {
-                DateFormat df = new SimpleDateFormat("HH:mm");
-                String time = df.format(Calendar.getInstance().getTime());
-                people.CameTime = time;
-                dbHelper.InsertCameTime(people.Name, time, YEAR, MONTH, DAY);
-                adapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void InsertLeaveTimeInData(People people) {
-                DateFormat df = new SimpleDateFormat("HH:mm");
-                String time = df.format(Calendar.getInstance().getTime());
-                people.LeaveTime = time;
-                dbHelper.InsertLeaveTime(people.Name, time, YEAR, MONTH, DAY);
-                adapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void CheckTime(People people) {
-                SQLiteDatabase sqLiteDatabase = dbHelper.getReadableDatabase();
-                String id = dbHelper.GetIdByName(people.Name);
-
-                SetTimeToPeople(id,people);
-            }
-        });
-
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, PeopleActivity.class);
-                intent.putExtra("year", String.valueOf(YEAR));
-                intent.putExtra("month", String.valueOf(MONTH));
-                intent.putExtra("day", String.valueOf(DAY));
-                startActivity(intent);
-                //update();
-            }
-        });
-
-        mcv.setOnMonthChangedListener(new OnMonthChangedListener() {
-            @Override
-            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
-                MONTH = String.valueOf(date.getMonth());
-                eventDecorator.setDates(dbHelper.SelectAllNotEmptyDays(YEAR,MONTH));
-                mcv.invalidateDecorators();
-            }
-        });
-
-        mcv.setOnDateChangedListener(new OnDateSelectedListener() {
-            @Override
-            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
-                YEAR = String.valueOf(date.getYear());
-                MONTH = String.valueOf(date.getMonth());
-                DAY = String.valueOf(date.getDay());
-                update();
-            }
-        });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        update();
-        eventDecorator.setDates(dbHelper.SelectAllNotEmptyDays(YEAR,MONTH));
-        mcv.invalidateDecorators();
-    }
-
-    private void update() {
-        FindIDPeopleByData();
-        if (adapter != null)
-        {
-            adapter.notifyDataSetChanged();
-        }
-    }
-
-
-    private void FindIDPeopleByData() {
-        peopleList.clear();
-        ArrayList<String> id = new ArrayList<>();
-        SQLiteDatabase sqLiteDatabase = dbHelper.getReadableDatabase();
-
-        Cursor c = sqLiteDatabase.rawQuery("SELECT " + dbHelper.ID_PEOPLE
-                        + " FROM " + dbHelper.DATA_PEOPLE 
-                        + " WHERE " + dbHelper.YEAR + " = ?" 
-                        + " AND " + dbHelper.MONTH + " = ?" 
-                        + " AND " + dbHelper.DAY + " = ?"
-                , new String[]{YEAR, MONTH, DAY});
-        if (c.moveToFirst()) {
-            do {
-                String index = c.getString(c.getColumnIndex(dbHelper.ID_PEOPLE));
-                id.add(index);
-            } while (c.moveToNext());
-        } else
-            Log.d(LOG_TAG, "Cursor is null");
-        c.close();
-        FindNamePeopleById(id);
-        sqLiteDatabase.close();
-    }
-
-    private void FindNamePeopleById(ArrayList<String> id) {
-        SQLiteDatabase sqLiteDatabase = dbHelper.getReadableDatabase();
-        Cursor cursor;
-
-        for (String s : id) {
-            cursor = sqLiteDatabase.rawQuery("SELECT " + dbHelper.FULL_NAME
-                    + " FROM " + dbHelper.PEOPLE
-                    + " WHERE " + dbHelper.KEY_ID 
-                    + " = ?", new String[]{s});
-            if (cursor.moveToFirst()) {
-                do {
-                    String name = cursor.getString(cursor.getColumnIndex(dbHelper.FULL_NAME));
-                    People people = new People(name);
-                    people.Group = dbHelper.FindGroupThisPeople(people);
-                    peopleList.add(people);
-                } while (cursor.moveToNext());
-            } else
-                Log.d(LOG_TAG, "Cursor is null");
-        }
-        sqLiteDatabase.close();
-    }
-
-    private void SetTimeToPeople(String id,People people){
-        SQLiteDatabase sqLiteDatabase = dbHelper.getReadableDatabase();
-        Cursor cursor;
-
-        cursor = sqLiteDatabase.rawQuery("SELECT " + dbHelper.CAME_TIME
-                + " FROM " + dbHelper.DATA_PEOPLE
-                + " WHERE "
-                + dbHelper.ID_PEOPLE + "=? "
-                + "AND " + dbHelper.YEAR + "=? "
-                + "AND " + dbHelper.MONTH + "=? "
-                + "AND " + dbHelper.DAY + "=? ", new String[]{id, YEAR, MONTH, DAY});
-        if (cursor.moveToFirst()) {
-            int index = cursor.getColumnIndex(dbHelper.CAME_TIME);
-            do {
-                people.CameTime = (cursor.getString(index));
-                //Log.d("people:", "Came " + people.CameTime + " Leave " + people.LeaveTime);
-            } while (cursor.moveToNext());
-        }
-
-        cursor = sqLiteDatabase.rawQuery("SELECT " + dbHelper.LEAVE_TIME
-                + " FROM " + dbHelper.DATA_PEOPLE
-                + " WHERE "
-                + dbHelper.ID_PEOPLE + "=? "
-                + "AND " + dbHelper.YEAR + "=? "
-                + "AND " + dbHelper.MONTH + "=? "
-                + "AND " + dbHelper.DAY + "=? ", new String[]{id, YEAR, MONTH, DAY});
-        if (cursor.moveToFirst()) {
-            int index = cursor.getColumnIndex(dbHelper.LEAVE_TIME);
-            do {
-                people.LeaveTime = (cursor.getString(index));
-                //Log.d("people:", "Came " + people.CameTime + " Leave " + people.LeaveTime);
-            } while (cursor.moveToNext());
-        }
-            cursor.close();
-            sqLiteDatabase.close();
-        }
-
-    public class EventDecorator implements DayViewDecorator {
-
-        private final int color;
-        private HashSet<CalendarDay> dates;
-
-        public EventDecorator(int color) {
-            this.color = color;
-        }
-
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
-        public boolean shouldDecorate(CalendarDay day) {
-            return dates.contains(day);
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navCalendar:
+                    fragmentManager.beginTransaction().hide(active).show(calendar).commit();
+                    active = calendar;
+                    return true;
+                case R.id.navPeoples:
+                    // fragmentManager.beginTransaction().hide(active).show(peopleFragment).commit();
+                    // active = peopleFragment;
+                    return true;
+                case R.id.navGroups:
+                    fragmentManager.beginTransaction().hide(active).show(groupsFragment).commit();
+                    active = groupsFragment;
+                    return true;
+            }
+            return false;
         }
-
-        @Override
-        public void decorate(DayViewFacade view) {
-            view.addSpan(new DotSpan(5, color));
-        }
-
-        public void setDates(Collection<CalendarDay> dates){
-            this.dates = new HashSet<>(dates);
-        }
-    }
-
+    };
 }
